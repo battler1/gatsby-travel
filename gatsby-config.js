a7fc8460a6f5cde598e063f655ca9c4f7036c899
battler1 <bdsoldire@gmail.com>
2021-06-22 14:14:24 +0600

    secound commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Travel`,
+    title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@battler`,
+    author: `@gatsbyjs`,
   },
-  pathPrefix: "/reponame",
+  pathPrefix: "/gatsby-travel",
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
